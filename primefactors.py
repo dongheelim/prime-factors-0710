c95cd2325655fc2a7e7a7ad4d3816c890768437b
donghee.lim <ldh5730@naver.com>
2024-07-10 09:02:27 +0900

    [feature] 3

--- primefactors.py
+++ primefactors.py
@@ -1,6 +1,6 @@
 class PrimeFactors:
     def of(self, number):
         factors = []
-        if number == 2:
+        if number == 2 or number == 3:
             factors.append(number)
         return factors
--- test_primefactors.py
+++ test_primefactors.py
@@ -8,6 +8,8 @@ class TestPrimeFactors(TestCase):
         self.assertEqual([], self.prime_factor.of(1))
     def test_prime_factor_of_2(self):
         self.assertEqual([2], self.prime_factor.of(2))
+    def test_prime_factor_of_3(self):
+        self.assertEqual([3], self.prime_factor.of(3))
 
     def setUp(self):
         super().setUp()
