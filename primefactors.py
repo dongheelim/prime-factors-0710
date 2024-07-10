bde8e5b58eb86235c1fc15b2a4fa9bdf9c7b8c8d
donghee.lim <ldh5730@naver.com>
2024-07-10 09:37:07 +0900

    [refactor] 6

--- primefactors.py
+++ primefactors.py
@@ -3,19 +3,12 @@ class PrimeFactors:
         factors = []
         divisor = 2
         if number >= divisor:
-            if number == 4:
-                while number % divisor == 0:
-                    factors.append(divisor)
-                    number /= divisor
-            elif number == 6:
+            if number == 4 or number == 6 or number == 9:
                 while number > 1 :
                     while number % divisor == 0:
                         factors.append(divisor)
                         number /= divisor
                     divisor += 1
-            elif number == 9:
-                factors.append(3)
-                factors.append(3)
             else:
                 factors.append(number)
         return factors
