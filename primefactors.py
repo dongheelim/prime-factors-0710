c27d335413803bb576e1c6194c8dee9b3ab2f872
donghee.lim <ldh5730@naver.com>
2024-07-10 09:06:22 +0900

    [refactor] 4

--- primefactors.py
+++ primefactors.py
@@ -3,8 +3,10 @@ class PrimeFactors:
         factors = []
         if number >= 2:
             if number == 4:
-                factors.append(2)
-                factors.append(2)
+                while number % 2 == 0:
+                    factors.append(2)
+                    number /= 2
+
             else:
                 factors.append(number)
         return factors
